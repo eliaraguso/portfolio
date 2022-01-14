829da037e5a8f6f637154b1ce524a2db59948c43
eliaraguso <elia.raguso@gmail.com>
2022-01-14 10:53:36 +0100

    aggiunto link a Deliveboo nella pagina projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,14 +1,15 @@
 <template>
   <main>
     <h2><a href="https://htmlcssdiscord.netlify.app/">Html + css Discord</a></h2>
+    <h2><a href="https://bootstrapfreelancer.netlify.app/">Bootstrap simple portfolio</a></h2>
+    <h2><a href="https://htmlcssdropbox.netlify.app/">Html + css Dropbox</a></h2>
     <h2><a href="https://htmlcssplaystation.netlify.app/">Html + css Playstation</a></h2>
     <h2><a href="https://vuedccomics.netlify.app/">Vue DC Comics Html+css</a></h2>
     <h2><a href="https://vueboolflix.netlify.app/">Vue Netflix</a></h2>
     <h2><a href="https://vuespotifyboolean.netlify.app/">Vue Spotify</a></h2>
-    <h2><a href="https://htmlcssdropbox.netlify.app/">Html + css Dropbox</a></h2>
-    <h2><a href="https://vuehealthcare.netlify.app/">Vue Healtcare</a></h2>
-    <h2><a href="https://bootstrapfreelancer.netlify.app/">Bootstrap simple portfolio</a></h2>
     <h2><a href="https://jscampominato.netlify.app/">JS campo minato</a></h2>
+    <h2><a href="https://vuehealthcare.netlify.app/">Vue Healtcare</a></h2>
+    <h2><a href="https://deliveboo-elia.herokuapp.com/">Laravel + Vue Ecommerce</a></h2>
   </main>
 </template>
 
